f026996b6d2435f2477fce4dcc83830c5bcd3b74
nnsahu02 <nnsahu2022@gmail.com>
2022-10-25 14:53:03 +0530

    bookschema Assignment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema({
     bookName: String,
     authorName: String,
     category: String,
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('Book', bookSchema)
 
 
 
