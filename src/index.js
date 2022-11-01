bb652a601e747ca5823e8ae30be7cbe82da0ae1e
nnsahu02 <nnsahu2022@gmail.com>
2022-11-01 23:55:51 +0530

    middleware assignment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment = require("moment");
+const { json } = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,8 +13,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://nnsahu2022:Sahurk012@mycluster.ne522qz.mongodb.net/Rahul", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb App ke seba mai hajir hai !"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb App ke seba mai hajir hai !"))
+    .catch(err => console.log(err))
+
+    // GLOBAL MIDDLEWARE //
+app.use(
+    function (req, res, next) {
+        const today = moment()
+        const ip = req.ip
+        const routing = req.originalUrl
+
+        console.log(today.format("YYYY-MM-DD, h:mm:ss a"), ip, routing);
+        next();
+    }
+);
+
+app.use('/', route);
 
 app.use('/', route);
 
