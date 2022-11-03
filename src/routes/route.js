bb099ff8dfa2682d1fd4d35de3540bb413bce711
nnsahu02 <nnsahu2022@gmail.com>
2022-11-04 00:00:19 +0530

    middleware 2 assignment solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+const userController = require("../controllers/userController")
+const productController = require("../controllers/productController")
+const orderController = require("../controllers/orderController")
+const validatorMiddleware = require("../middlewares/validator.js")
 
-//Can we set the 'next' input parameter in a route handler?
-//What is the primary difference between a middleware and a route handler?
-router.post("/createBook", commonMW.myMiddleware,BookController.createBook, function(req, res, next){
-    res.send("Ending the cycle")
-}  )
 
-router.post("/createUser", commonMW.myMiddleware, UserController.createUser)
-
-router.get("/dummy1", commonMW.myOtherMiddleware, UserController.dummyOne)
-
-router.get("/dummy2", commonMW.myOtherMiddleware, UserController.dummyTwo)
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+router.post("/createProduct", productController.createProduct)
+router.post("/createUser", validatorMiddleware.validateHeader, userController.createUser)
+router.post("/creatorder", validatorMiddleware.validateHeader, orderController.createOrder)
 
 module.exports = router;
